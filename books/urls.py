650003bfe4f4a58d3412f4299702989d89cec0db
Kukil Bora <kukil.bora@clearesult.com>
2025-06-17 23:07:54 +0530

    bookstore api app

--- books/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BookList
-
-urlpatterns = [
-    path('books/', BookList.as_view() , name="book-list"), # for GET and POST
-    path('books/<int:pk>/', BookList.as_view(), name="book-modify")# for PUT and DELETE
-]
